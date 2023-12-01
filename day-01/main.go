6bf53ab7096ab7cb32df7edd36c05a6e8c71d44a
RyanTalbot <ryan-talbot@outlook.com>
2023-12-01 21:45:24 +0000

    First working solution part 1

--- day-01/main.go
+++ day-01/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("day 1")
+	var sumArray []int
+	var sum int
+
+	input, _ := os.ReadFile("./day-01/input.txt")
+	lines := strings.Fields(string(input))
+
+	for line := range lines {
+		reg := regexp.MustCompile(`[^0-9]+`).ReplaceAllString(lines[line], "")
+		sumArray = append(sumArray, sumFirstAndLast(reg))
+	}
+
+	for number := range sumArray {
+		sum += sumArray[number]
+	}
+
+	fmt.Println(sumArray)
+	fmt.Println(sum)
+
+}
+
+func sumFirstAndLast(str string) int {
+	var first, last int
+
+	if len(str) > 0 {
+		if len(str) == 1 {
+			first = parse(str, 0)
+			last = parse(str, 0)
+		} else {
+			first = parse(str, 0)
+			last = parse(str, len(str)-1)
+		}
+
+	}
+
+	merge := fmt.Sprintf("%d%d", first, last)
+	result, _ := strconv.Atoi(merge)
+
+	return result
+}
+
+func parse(str string, pos int) int {
+	element := str[pos]
+	stringified := string(element)
+	result, _ := strconv.Atoi(stringified)
+
+	return result
 }
